29251c3d4a42592380dbc8bfee8381e614ce76ee
bong-suk <greedi9468@hanmail.net>
2025-02-06 02:53:52 +0900

    feature/regactor

--- main project/src/components/dropdown.jsx
+++ main project/src/components/dropdown.jsx
@@ -17,7 +17,12 @@ const Dropdown = ({ isOpen, onMouseEnter, onMouseLeave }) => {
         {isOpen && (
           <div className="dropdown-menu">
             <div className="menu-items">
-              <Link to={"/"}>홈</Link>
+              <div className="login" onClick={() => setModalOpen(true)}>
+                로그인
+              </div>
+              <Link to={"/"} className="menu-item">
+                메인
+              </Link>
               <div className="menu-item">마이페이지</div>
               <Link to={"/travel-diary"} className="menu-item">
                 여행기록
@@ -25,10 +30,6 @@ const Dropdown = ({ isOpen, onMouseEnter, onMouseLeave }) => {
               <Link to={"/travelInformation"} className="menu-item">
                 여행정보
               </Link>
-              <div className="menu-divider"></div>
-              <div className="menu-item" onClick={() => setModalOpen(true)}>
-                로그인하기
-              </div>
             </div>
           </div>
         )}
