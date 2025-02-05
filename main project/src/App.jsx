efe732306bd7ccd1377581eaadea6b835a135fc5
newwmee <dmstjs365@gmail.com>
2025-02-05 15:09:20 +0900

    feat:storyboard라이브러리구현

--- main project/src/App.jsx
+++ main project/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./pages/mainpage.jsx";
 import TravelDiaryPage from "./pages/Traveldiarypage";
+import Travelhistory from "./components/Travelhistory.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/travel-diary" element={<TravelDiaryPage />} />
+        <Route path="/write-post" element={<Travelhistory />} />
       </Routes>
     </Router>
   );
