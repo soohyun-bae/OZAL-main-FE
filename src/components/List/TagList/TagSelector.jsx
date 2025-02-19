57cd39b1efa8e2cd287184d6ba78a0eb7c996778
MyangSZ <unoyou@naver.com>
2025-02-19 16:08:00 +0900

    DesignSCSS

--- src/components/List/TagList/TagSelector.jsx
+++ src/components/List/TagList/TagSelector.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import '../../../style/travelInfopage.scss';
+import "../../../style/travelInfopage.scss";
 import useMediaQuery from "../../../hooks/useMediaQuery";
 import Modal from "../../Modal/Modal";
 import { openModal, closeModal } from "../../../RTK/modalSlice";
 import TagList from "./TagList";
-import ListStyle from '.././List.module.scss'
+import ListStyle from ".././List.module.scss";
 import { fetchCityName } from "../../../RTK/tour/thunk";
 
 const TagSelector = () => {
@@ -21,16 +21,25 @@ const TagSelector = () => {
     <>
       {isSmallView ? (
         <>
-          <div onClick={() => dispatch(openModal("tags"))}>목록보기</div>
+          <div
+            onClick={() => dispatch(openModal("tags"))}
+            className={ListStyle["sTaglist"]}
+          >
+            목록보기
+          </div>
           {isModalOpen && (
             <Modal type="tags">
-              <TagList />
-              <div onClick={() => dispatch(closeModal("tags"))}>닫기</div>
+              <div className={ListStyle["modalList"]}>
+                <TagList />
+                <div onClick={() => dispatch(closeModal("tags"))}>
+                  <button>닫기</button>
+                </div>
+              </div>
             </Modal>
           )}
         </>
       ) : (
-        <div className={ListStyle['tag-container']}>
+        <div className={ListStyle["tag-container"]}>
           <TagList />
         </div>
       )}
--- src/components/Modal/LoginModal.jsx
+++ src/components/Modal/LoginModal.jsx
@@ -2,7 +2,7 @@
 import "../../style/modalStyle.scss";
 import kakaoLogin from "../../assets/kakao_login.png";
 import naverLogin from "../../assets/naver_login.png";
-import waypoint from "../../assets/waypoint.png";
+import waypoint from "../../assets/mainLogo.png";
 import "../../App.css";
 import Modal from "./Modal";
 import { useDispatch, useSelector } from "react-redux";
